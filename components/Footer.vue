33169c7937e4b863e17e62221b499022efeab78e
Nicolas <it@propulse-lab.com>
2021-11-30 21:58:42 +0100

    CHANGES, footer update

--- components/Footer.vue
+++ components/Footer.vue
@@ -4,10 +4,7 @@
     <div class="md:py-5">
       <div class="flex flex-col items-center justify-between mt-6 md:mt-0 md:flex-row">
         <div class="logo flex items-center">
-          <p class="text-sm text-gray-700 dark:text-gray-200 mx-1">© 2021 {{ $config.githubUsername }} Built with ♡ </p>
-          <a
-aria-label="AymaneMx.com" class="text-sm underline text-gray-700 dark:text-gray-200"
-             href="https://github.com/aymaneMx/nuxt-portfolio-dev">Open sourced on Github</a>
+          <p class="text-sm text-gray-700 dark:text-gray-200 mx-1">© 2021 {{ $config.githubUsername }} 🤟🏼 </p>
         </div>
 
 
